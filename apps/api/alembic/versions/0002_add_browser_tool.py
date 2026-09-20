9a4485770dd45213436a76e7b9492b57e9e40beb
TunaYmc <tunayamacc@gmail.com>
2026-09-20 10:36:08 +0000

    fix: correct alembic down_revision

--- apps/api/alembic/versions/0002_add_browser_tool.py
+++ apps/api/alembic/versions/0002_add_browser_tool.py
@@ -10,8 +10,8 @@ import sqlalchemy as sa
 from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
-revision = '0002'
-down_revision = '0001'
+revision = '0002_add_browser_tool'
+down_revision = '0001_initial_schema'
 branch_labels = None
 depends_on = None
 
